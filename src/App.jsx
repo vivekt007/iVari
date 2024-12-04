e65fbe043ecb6aa7f0be31f3a9f3cf95b6cdcc0e
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 10:37:25 +0530

    "Update homepage URL and route paths in package.json, App.jsx, and Navbar.jsx; change base path in vite.config.js"

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
             <ScrollToTop />
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/services" element={<Services />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/groupCompanies" element={<GroupCompanies />} />
