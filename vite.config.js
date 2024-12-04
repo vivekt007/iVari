1a6623001d3dc875b3d67ff006eaee119ec000da
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 15:43:57 +0530

    "Update homepage URL in package.json and adjust base path in vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/iVari/",
-    // Base path for GitHub Pages
+    base: "/iVari/", // Base path for GitHub Pages
     server: {
         port: 3000,
         host: "0.0.0.0",
