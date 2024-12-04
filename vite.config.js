d05328f3d881fc8bac236246cf6c7af8013677ab
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 15:41:32 +0530

    ....

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/iVari/", // Base path for GitHub Pages
+    base: "/iVari/",
+    // Base path for GitHub Pages
     server: {
         port: 3000,
         host: "0.0.0.0",
